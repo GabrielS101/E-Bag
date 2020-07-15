a03a453e19066b2d179c9f6e0cf8201cc758f0a3
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-07-15 17:43:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ bot.on('message', message=>{
             .addField("Remote Controlled Mode", '5 Turn Wait/Cooldown - Makes You Invincible for 1 turn.')
             .addField("Illusion", '4 Turn Cooldown - Roll a 1-10 dice. If you roll a 4 or Lower than take 100 damage. If you roll 5 or higher deal 100 damage.')
             .addField("Plankton Heal", '5 Turn Cooldown - Heal 100 Health.')
+            .addImage("https://vignette.wikia.nocookie.net/jjba/images/d/dc/Howaitosuneiku.png/revision/latest?cb=20190829041008")
             .setColor(0xb5b3b3)
             message.channel.send(Whitesnake);
          break;        
@@ -73,6 +74,7 @@ bot.on('message', message=>{
             .addField("Gravitational Wall Slam", '150 - 4 Turn Cooldown.')
             .addField("Gravitational Force Field", '4 Turn Wait/Cooldown - Block Your Opponents Next Attack.')
             .addField("Passive - Gravitational Bending", 'Your attacks cannot be dodged but can be blocked.')
+            .addImage("https://i.pinimg.com/originals/81/c8/f0/81c8f0bdef87eca7b4ab28267e2762f7.png")
             .setColor(0x34a857)
             message.channel.send(CMoon);
          break;  
