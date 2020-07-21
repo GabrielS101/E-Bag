9d741eb6f5c5aec6fc2083dec64c5272e5ae7aac
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-07-21 03:52:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -191,7 +191,7 @@ bot.on('message', message=>{
          case 'THTW':  
          const TheHandTheWorld = new Discord.MessageEmbed()             
          .setTitle('The Hand: The World')
-         .setDescription("This Stand Is A Fusion Between The Hand And The World.")
+         .setDescription("This Stand Is The Exclusive Property Of <@502242566272581662>.")
          .addField('Health', '900.')
          .addField('Punch', '80.')
          .addField("Timestop", "5 Turn Cooldown - Stun your opponent for the next 2 turns.")
