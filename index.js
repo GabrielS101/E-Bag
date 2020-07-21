89dd5d154ffad5b344591166d31259e3a9794995
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-07-21 03:49:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -142,6 +142,7 @@ bot.on('message', message=>{
         case 'Metallica':  
          const Metallica = new Discord.MessageEmbed()             
          .setTitle('Metallica')
+         .setDescription("This Stand Is The Exclusive Property Of <@340100783901245441>")
          .addField('Health', '800.')
          .addField('Nail', '70.')
          .addField("Invisibility", "3 Turn Cooldown - Dodge Your Opponents Next Attack.")
@@ -167,6 +168,7 @@ bot.on('message', message=>{
         case 'Mr.President':  
          const MrPresident = new Discord.MessageEmbed()             
          .setTitle('Mr.President')
+         .setDescription("This Stand Is The Exclusive Property Of <@592471909707546634>")
          .addField('Health', '1000.')
          .addField('Bite', '60.')
          .addField("Shell Spin", "4 Turn Cooldown - 80x6.")
