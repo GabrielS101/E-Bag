b75e156baf9aa6dd4ea9b90317ece2ea50897102
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-07-24 18:06:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,39 @@ bot.on('ready', () =>{
 
 bot.login(process.env.token);
 
+exports.run = (client, message, args, tools) => {
+    let pages = ['1: Purple Haze Distortion.', '2: Hermit Purple.', '3: White Album.'];
+    let page = 1;
+            message.channel.send(PublicStands1).then(msg => {
+                msg.react('◀️').then( r => {
+                    msg.react('▶️')
+
+                    const backwardsFilter = (reaction, user) => reaction.emoji.name === '◀️' && user.id === message.author.id;
+                    const forwardsFilter = (reaction, user) => reaction.emoji.name === '▶️' && user.id === message.author.id;
+
+                    const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 });
+                    const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 });
+
+                    backwards.on('collect', r => {
+                        if (page === 1) return;
+                        page--;
+                        embed.SetDescription(pages[page-1]);
+                        embed.setFooter(`Page ${page} of ${pages.length}`);
+                        message.edit(PublicStands1)
+                    })
+
+                    forwards.on('collect', r => {
+                        if (page === pages.lenght) return;
+                        page++;
+                        embed.SetDescription(pages[page-1]);
+                        embed.setFooter(`Page ${page} of ${pages.length}`);
+                        message.edit(PublicStands1)
+                    })
+                })
+            })
+
+}
+
 const PREFIX ='E-'
 
 bot.on('message', message=>{
