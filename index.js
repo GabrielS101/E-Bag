668c81a25afb702af337a62f8f2dcdbea8fb21ad
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-07-24 18:25:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,49 +9,47 @@ const PREFIX = 'E-'
 
 bot.login(process.env.token);
 
-exports.run = (client, message, args, tools) => {
-    let pages = ['Page 1.', 'Page 2.', 'Page 3'];
-    let page = 1;
-            const embed = new Discord.MessageEmbed
-            .setColor(0xffffff)
-            .setFooter(`Page ${page} of ${pages.length}`)
-            .setDescription(pages[page-1])
-            message.channel.send(embed).then(msg => {
-                msg.react('◀️').then( r => {
-                    msg.react('▶️')
-
-                    const backwardsFilter = (reaction, user) => reaction.emoji.name === '◀️' && user.id === message.author.id;
-                    const forwardsFilter = (reaction, user) => reaction.emoji.name === '▶️' && user.id === message.author.id;
-
-                    const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 });
-                    const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 });
-
-                    backwards.on('collect', r => {
-                        if (page === 1) return;
-                        page--;
-                        embed.SetDescription(pages[page-1]);
-                        embed.setFooter(`Page ${page} of ${pages.length}`);
-                        message.edit(embed)
-                    })
-
-                    forwards.on('collect', r => {
-                        if (page === pages.lenght) return;
-                        page++;
-                        embed.SetDescription(pages[page-1]);
-                        embed.setFooter(`Page ${page} of ${pages.length}`);
-                        message.edit(embed)
-                    })
-                })
-            })
-
-}
-
-const PREFIX ='E-'
-
 bot.on('message', message=>{
 
     let args = message.content.slice(PREFIX.length).split(" ");
 
+    exports.run = (client, message, args, tools) => {
+        let pages = ['Page 1.', 'Page 2.', 'Page 3'];
+        let page = 1;
+                const embed = new Discord.MessageEmbed
+                .setColor(0xffffff)
+                .setFooter(`Page ${page} of ${pages.length}`)
+                .setDescription(pages[page-1])
+                message.channel.send(embed).then(msg => {
+                    msg.react('◀️').then( r => {
+                        msg.react('▶️')
+    
+                        const backwardsFilter = (reaction, user) => reaction.emoji.name === '◀️' && user.id === message.author.id;
+                        const forwardsFilter = (reaction, user) => reaction.emoji.name === '▶️' && user.id === message.author.id;
+    
+                        const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 });
+                        const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 });
+    
+                        backwards.on('collect', r => {
+                            if (page === 1) return;
+                            page--;
+                            embed.SetDescription(pages[page-1]);
+                            embed.setFooter(`Page ${page} of ${pages.length}`);
+                            message.edit(embed)
+                        })
+    
+                        forwards.on('collect', r => {
+                            if (page === pages.lenght) return;
+                            page++;
+                            embed.SetDescription(pages[page-1]);
+                            embed.setFooter(`Page ${page} of ${pages.length}`);
+                            message.edit(embed)
+                        })
+                    })
+                })
+    
+    }
+        
     switch(args[0]){
         case 'PurpleHazeDistortion':
             const PurpleHazeDistortion = new Discord.MessageEmbed()                //this command is if you want embed with prefix and no ping
