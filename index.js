5cf014202a9351247936be0d282b865c19681c61
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-07-25 18:52:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,43 +9,6 @@ bot.login(process.env.token);
 
 const PREFIX ='E-'
 
-exports.run = (client, message, args, tools) => {
-    let pages = ['Page 1.', 'Page 2.', 'Page 3'];
-    let page = 1;
-            const embed = new Discord.MessageEmbed
-            .setColor(0xffffff)
-            .setFooter(Page ${page} of ${pages.length})
-            .setDescription(pages[page-1])
-            message.channel.send(embed).then(msg => {
-                msg.react('arrow_backward').then( r => {
-                    msg.react('arrow_forward')
-
-                    const backwardsFilter = (reaction, user) => reaction.emoji.name === 'arrow_backward' && user.id === message.author.id;
-                    const forwardsFilter = (reaction, user) => reaction.emoji.name === 'arrow_forward' && user.id === message.author.id;
-
-                    const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 });
-                    const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 });
-
-                    backwards.on('collect', r => {
-                        if (page === 1) return;
-                        page--;
-                        embed.SetDescription(pages[page-1]);
-                        embed.setFooter(Page ${page} of ${pages.length});
-                        message.edit(embed)
-                    })
-
-                    forwards.on('collect', r => {
-                        if (page === pages.lenght) return;
-                        page++;
-                        embed.SetDescription(pages[page-1]);
-                        embed.setFooter(Page ${page} of ${pages.length});
-                        message.edit(embed)
-                    })
-                })
-            })
-
-}
-
 bot.on('message', message=>{
 
     let args = message.content.slice(PREFIX.length).split(" ");
