78f69a746b49611e0e9499623f490b2244bbf4b7
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-07-27 16:15:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,10 +16,14 @@ const PREFIX ="E-"
 bot.on('message', message=>{
 
     module.exports.run = async (bot, message, args) => {
+
+        //!tempmute @user 1s/m/h/d
+      
         let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        if(!tomute) return message.reply("Member Not Found In This Server.");
-        if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Member Can Not Be Muted");
-        let muterole = message.guild.roles.find(muterole => muterole.name === "Muted");
+        if(!tomute) return message.reply("Couldn't find user.");
+        if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
+        let muterole = message.guild.roles.find(`name`, "muted");
+        //start of create role
         if(!muterole){
           try{
             muterole = await message.guild.createRole({
@@ -37,17 +41,24 @@ bot.on('message', message=>{
             console.log(e.stack);
           }
         }
+        //end of create role
         let mutetime = args[1];
-        if(!mutetime) return message.reply("Time Not Specified");
+        if(!mutetime) return message.reply("You didn't specify a time!");
+      
         await(tomute.addRole(muterole.id));
-        message.reply(`<@${tomute.id}> Has Been Muted For ${ms(ms(mutetime))}`);
+        message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+      
         setTimeout(function(){
           tomute.removeRole(muterole.id);
-          message.channel.send(`<@${tomute.id}> Has Been Unmuted`);
+          message.channel.send(`<@${tomute.id}> has been unmuted!`);
         }, ms(mutetime));
+      
+      
+      //end of module
       }
+      
       module.exports.help = {
-        name: "mute"
+        name: "tempmute"
       }
     let args = message.content.slice(PREFIX.length).split(" ");
     switch(args[0]){
