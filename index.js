05b448ffb2e679fc5d0b7f2b2526901766bcc89c
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-07-27 04:29:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,26 @@ bot.on('message', message=>{
                 }
             }
        break;
+       case 'ban':
+            if(!args[1]) message.channel.send('No Member Specified')
+
+            const user = message.mentions.members.first();
+
+            if(user){
+                const member = message.guild.member(user);
+
+                if(member){
+                    member.ban({reason: 'Banned'}).then(() =>{
+                        message.reply('Succesfully Banned Member');
+                    }).catch(err =>{
+                        message.reply('Unable To Ban Member');
+                        console.log(err);
+                    });
+                } else{
+                    message.reply("Member Not Found In This Server")
+                }
+            }
+       break;
         case 'PublicStands1':
             const PublicStands1 = new Discord.MessageEmbed()
             .setTitle('Public Stands 1')
