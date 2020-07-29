62a8ab523b0a602aeb55ffe1ff4350680450fd63
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-07-29 15:42:57 -0400

    Summary

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const ms = require('ms');
+const MusicClient = require("discord-music-core");
+
+const musicPlayer = new MusicClient("Youtube Api key");
+musicPlayer.play(message, searchArray) // Searches for and add the searched results to the queue.
+musicPlayer.playTop(message, searchArray) // Searches for and add the searched results to the top of the queue.
+musicPlayer.stop(message) // Stop the music, which includes clearing the queue.
+musicPlayer.nowPlaying(message) // Display the music title for now playing.
+musicPlayer.showQueue(message) // Displays the whole music queue.
+musicPlayer.skip(message) // Skip the song that is now playing.
+musicPlayer.remove(message) // Removes certain position of song in the song queue.
+musicPlayer.pause(message) // Pauses the music queue.
+musicPlayer.repeat(message) // Repeats the current song.
+musicPlayer.loop(message) // Loops the whole queue.
+musicPlayer.shuffle(message) // Shuffles the whole queue.
+musicPlayer.volume(message, volume) // Sets the volume to the certain amount. Using negative values will show the volume that is using now instead.
 
 module.exports.run = async (bot, message, args) => {
 
