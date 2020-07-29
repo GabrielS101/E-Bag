95954ff2348f6786989513dd772974c5dd4ac32d
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-07-29 15:37:08 -0400

    Summary

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const ms = require('ms');
-const { CommandoClient } = require('discord.js-commando');
-const path = require('path');
-const config = require( path.resolve( __dirname, "config.json" ) );
-
-const client = new CommandoClient({
-    commandPrefix: config.prefix,
-    unknownCommandResponse: false,
-    owner: config.owner,
-    disableEveryone: true
-});
-
-client.registry
-    .registerDefaultTypes()
-    .registerGroups([
-        ['polls', 'Polls'],
-    ])
-    .registerDefaultGroups()
-    .registerDefaultCommands()
-    .registerCommandsIn(path.join(__dirname, 'commands'));
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity(config.activity);
-});
-
 
 module.exports.run = async (bot, message, args) => {
 
