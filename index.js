de56176ef459a3981dbcfcf7d07c8e29db76bf89
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-08-01 19:52:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ client.on('message', async message => {
             .setDescription("**Daily Rewards**")
             .addField("Collected", amount)
             message.channel.send(dailyreward)
-
             db.add(`money_${message.author.id}`, amount)
             db.add(`daily_${message.author.id}`, Date.now())
         }
@@ -55,10 +54,8 @@ client.on('message', async message => {
             message.channel.send(`You Already Worked. Please Come Back In ${time.hours} Hours, ${time.minutes} Minutes, And ${time.seconds} Seconds`)
        }else {
            let amountearned = Math.floor(Math.random() *500) + 1
-
-           let jobs = ["Assasin", "Gangster", "Marine Biologist", "SpeedWagon Foundation Employee"]
+           let jobs = ["Assasin", "Gangster", "Marine Biologist", "SpeedWagon Foundation Employee", "Kame Yu Department Employee", "Manga Artist", "Fortune Teller", "Prison Guard", "The President", "Fruit Parlor Employee", "Priest"]
            let job = jobs[Math.floor(Math.random() *jobs.length)]
-
            let working = new Discord.MessageEmbed()
            .setAuthor(`${message.author.tag} Got Paid`, message.author.displayAvatarURL())
            .setDescription(`${message.author} Worked As A ${job} And Earned ${amountearned} Dollars`)
