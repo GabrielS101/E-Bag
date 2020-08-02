abc5c3e6ec0bdb20bf70c0663380e512c64a5dec
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-08-01 23:57:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.on('message', async message => {
     switch(args[0].toLowerCase()) {
         case 'balance':
         let user = message.mentions.users.first() || message.author
-        let money = db.fetch(`money_${user.id}`)
+        var money = db.fetch(`money_${user.id}`)
         if (money === null) money = 0
         message.channel.send(`${user} You Have ${money} Dollars`)
         break;
@@ -85,7 +85,7 @@ client.on('message', async message => {
         db.add(`money_${message.author.id}`, wantedamount) 
        }break;
        case 'remove':
-        let money = db.fetch(`money_${user.id}`)
+        var money = db.fetch(`money_${user.id}`)
         if(money < unwantedamount) {
             message.channel.send("You Dont Have That Much To Get Rid Of")
         }else {
