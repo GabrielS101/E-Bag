f011a2a09b8c029930b3a213fd20a298e34f0508
GabrielS101 <gabrielshteynberg@gmail.com>
2020-08-02 00:40:31 -0400

    g

--- index.js
+++ index.js
@@ -85,11 +85,12 @@ client.on('message', async message => {
         db.add(`money_${message.author.id}`, wantedamount) 
        }break;
        case 'remove':
+        let extra = (unwantedamount - money)
         var user = message.mentions.users.first() || message.author
         var money = db.fetch(`money_${user.id}`)
         let unwantedamount = (args[1])
         if(money < unwantedamount) {
-            message.channel.send("You Dont Have That Much To Get Rid Of")
+            message.channel.send(`You Dont Have That Much To Get Rid Of. You Went Over By ${extra}`)
         }else {
         if(!message.member.hasPermission("ADMINISTRATOR", explicit = true)) return message.channel.send('Only People With The Administrator Permission Can Use This Command');
         if(!unwantedamount === Number) {
