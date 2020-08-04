83a58652dab2fce2fe58903ca44882bb9acd0045
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-08-04 17:53:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -186,15 +186,18 @@ client.on('message', async message => {
         const Stands = ["Purple Haze Distortion", "Hermit Purple", "White Album","Heavens Door", "Soft And Wet", "Hierophant Green", "Enigma", "Sticky Fingers", "Star Platinum: The World", "Echoes Egg", "Silver Chariot", "The Fool", "The World: Alternate Universe", "Whitesnake", "Stray Cat", "Crazy Diamond", "The Hand", "Killer Queen", "Wheel Of Fortune", "Hanged Man", "Tower Of Gray", "Love Deluxe", "Geb", "Red Hot Chili Pepper", "Moody Blues", "Gold Experience", "Sex Pistols", "Emperor"]
         const randomlychosenstand = Stands[Math.floor(Math.random() * Stands.length)];
         var items = db.get(message.author.id)
+        if(items === null) { 
         const randomstands = new Discord.MessageEmbed()
         .setTitle(`You Got ${randomlychosenstand} As Your Stand. Congratulations`)
         .setAuthor(`You Got A New Stand`, message.author.displayAvatarURL())
         .setDescription("Your New Stand Can Be Found In Your Inventory")
         .setFooter(`Check Your New Stand's Stats By Doing e-${randomlychosenstand}`)
         .setColor("RANDOM")
-        message.channel.send(randomstands)
-        db.push(message.author.id, randomlychosenstand)
-        break;
+         message.channel.send(randomstands)
+        db.push(message.author.id, `${randomlychosenstand}`)
+    }else {
+        message.channel.send("You Already Have A Stand")
+       }break;
         case 'roka':
         var items = db.get(message.author.id)
         message.channel.send("You Used A Roka And Reset Your Stand")
