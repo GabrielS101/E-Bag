57a35c086e0f6fbeabe83ede1ed011d28753dac8
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-08-04 08:14:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -199,15 +199,16 @@ client.on('message', async message => {
         message.channel.send("You Already Have A Stand")
        }break;
         case 'roka':
+        message.channel.awaitMessages(m => m.author.id == message.author.id)
         var user = message.mentions.users.first() || message.author
         var money = db.fetch(`money_${user.id}`)
         var items = db.get(message.author.id)
        await message.channel.send("Are You Sure You Want To Reset Everything? Please Specify Yes Or No") 
-       if (message.author.first().content.toLowerCase() == 'yes') {
+       if (message.first().content.toLowerCase() == 'yes') {
         message.reply('You Ate The Roka And Reset Everything');
             db.delete(message.author.id, items)
             db.subtract(`money_${money}`)
-       } if (message.author.first().content.toLowerCase() == 'no') {
+       } if (message.first().content.toLowerCase() == 'no') {
         message.reply('You Decided Not To Eat The Roka And Kept What You Have');
        }break;
         case 'meme':
