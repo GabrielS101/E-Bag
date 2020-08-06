38a4b266a0ba984b167ecc3a683f313bca0f1780
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-08-05 20:59:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -355,21 +355,21 @@ client.on('message', async message => {
             var person = message.mentions.members.first();
             if(person){
                 var member = message.guild.member(user);
-                if(args[1] = member){
+                if(member){
                     member.kick('You have been kicked').then(() =>{
                         message.reply('Succesfully Kicked Member')
                         .then(message => message.delete({timeout: 5000}));
                     }).catch(err =>{
                         message.reply('Unable To Kick Member')
                         .then(message => message.delete({timeout: 5000}));
-                        console.log(err);
+                        console.log(error);
                     });
                 } else{
                     message.reply("Member Not Found In This Server")
                     .then(message => message.delete({timeout: 5000}));
+                    
                 }
-            }
-       break;
+      }break;
        case 'ban':
         if(!message.member.hasPermission("ADMINISTRATOR" && "BAN_MEMBERS", explicit = true)) return message.channel.send('Only People With The Administrator Permission Or The Ban Members Permission Can Use This Command').then(message => message.delete({timeout: 5000}));
             if(!args[1]) message.channel.send('No Member Specified')
