b8c8fca39551afbda56253b17c2b3caac88c9443
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-08-12 15:04:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ client.on('message', async message => {
     case 'gamble':
     var money = db.fetch(`money_${message.author.id}`)
     const bet = args[1]
+    if (!bet) return message.channel.send("Amount To Gamble Not Specified")
     if (bet > money) return message.channel.send("You Don't Have That Much Money To Gamble")
     if (isNaN(bet)) {
       message.channel.send("Amount To Gamble Must Be In Number Form")}
