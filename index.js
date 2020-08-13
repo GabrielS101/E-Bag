dd883f6c49c7a42d2ba23ecbe21ea3212b3e2733
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-08-13 04:14:09 -0400

    update index.js

--- index.js
+++ index.js
@@ -76,14 +76,17 @@ client.on('message', async message => {
         message.channel.send(loseamount)
         db.subtract(`money_${message.author.id}`, bet)
       }if (pick === 'jackpot') {
-        const jackpotbet = bet×5
         const jackpotamount = new Discord.MessageEmbed()
         .setTitle(`You Struck The Jackpot`)
         .setAuthor(`${message.author.username} Won The Jackpot`, message.author.displayAvatarURL())
-        .setDescription(`${jackpotbet} Dollars Have Been Added To Your Balance`)
+        .setDescription(`5 x ${bet} Dollars Have Been Added To Your Balance`)
         .setFooter("You Can Chech Your Balance By Doing e-Info")
         message.channel.send(jackpotamount)
-        db.add(`money_${message.author.id}`, jackpotbet)
+        db.add(`money_${message.author.id}`, bet)
+        db.add(`money_${message.author.id}`, bet)
+        db.add(`money_${message.author.id}`, bet)
+        db.add(`money_${message.author.id}`, bet)
+        db.add(`money_${message.author.id}`, bet)
       }break;
     case 'daily':
       let daily = await db.fetch(`daily_${message.author.id}`);
