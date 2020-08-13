e198f478f071e1e1c04af52941960968625fa809
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-08-13 04:07:11 -0400

    update index.js

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ client.on('message', async message => {
         message.channel.send(loseamount)
         db.subtract(`money_${message.author.id}`, bet)
       }if (pick === 'jackpot') {
-        const jackpotbet = bet(5)
+        const jackpotbet = bet×5
         const jackpotamount = new Discord.MessageEmbed()
         .setTitle(`You Struck The Jackpot`)
         .setAuthor(`${message.author.username} Won The Jackpot`, message.author.displayAvatarURL())
