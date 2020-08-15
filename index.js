42504419ee07d0baf86c4b91c5ae53ccecaa2037
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-08-15 00:55:52 -0400

    update index.js

--- index.js
+++ index.js
@@ -1333,10 +1333,14 @@ client.on('message', async message => {
       message.channel.send("I Love You Son")
       break;
     case 'bot':
-      if (args[1] = 'invite') return message.channel.send('https://discordapp.com/oauth2/authorize?client_id=736099696623353858&scope=bot&permissions=8')
+      if (args[1] = 'invite') { 
+        message.channel.send('https://discordapp.com/oauth2/authorize?client_id=736099696623353858&scope=bot&permissions=8')
+      }else return message.channel.send("Full Name Of Command Not Specified")
       break;
     case 'server':
-      if (args[1] = 'invite') return message.channel.send('https://discord.gg/6ueb6Yy')
+      if (args[1] = 'invite') { 
+        message.channel.send('https://discord.gg/6ueb6Yy')
+      }else return message.channel.send("Full Name Of Command Not Specified")
       break;
   }
 })
