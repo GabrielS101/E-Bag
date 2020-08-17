2c3ab08dbf41a1c9f8aecc41bccb5e70cdfb46b7
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-08-17 15:46:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ client.on('message', async message => {
     if (!permissions.has("SPEAK")) return message.channel.send("I Do Not Have Permission To Speak In This Voice Channel")
     const songInfo = await ytdl.getInfo(args[1])
     const song = {
-      title: songInfo.title,
-      url: songInfo.video_url
+      title: info.videoDetails.title,
+      url: info.videoDetails.video_url
     }
     if (!serverQueue) {
       const queueConstruct = {
