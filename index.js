370b0768ea366b246b53e109c789186a03212d6f
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-08-24 03:17:11 -0400

    update index.js

--- index.js
+++ index.js
@@ -44,9 +44,13 @@ client.on('message', async message => {
     case'simp':
     if (!args[1]) return message.channel.send("Simp Not Specified")
     var user = message.mentions.users.first()
+    var otheruser = message.mentions.users.second()
     if (!user) return message.channel.send("Simp Not Specified")
+    if (!otheruser && args[2] != 'for') {
     if (user.id != '340100783901245441') {message.channel.send(`${user.username} Is A Simp`)
-    }else message.channel.send("Mentioned Person Is Too Chad To Be A Simp")
+    }else message.channel.send("My Creator Is Too Chad To Be A Simp")
+    }else  (user.id != '340100783901245441') {message.channel.send(`${user.username} Is A Simp For ${otheruser.username}`)
+    }else message.channel.send("My Creator Is Too Chad To Be A Simp")
     break;
     case 'gamble':
       if (message.author.bot === true && message.author.id != '736099696623353858') return message.channel.send("Bots Can Not Gamble")
