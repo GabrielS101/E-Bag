fb17d4010e5284a14acd667a85d65b91b62f4b28
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-09-01 15:14:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1106,6 +1106,7 @@ client.on('message', async message => {
       .setURL(`https://reddit.com/r/${random}`)
       message.channel.send(meme)
     }else {const chosenimg = await randomPuppy(chosensubreddit);
+    if (post.nsfw === true && message.channel.nsfw === false) return message.channel.send('This Is Not an NSFW channel') 
     if (!chosenimg) return message.channel.send("SubReddit Not Found")
     const chosenmeme = new Discord.MessageEmbed()
     .setColor("RANDOM")
