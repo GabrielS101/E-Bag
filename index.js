3991567a06f39db9cb5655b620500ec34843dc19
GabrielS101 <66330418+GabrielS101@users.noreply.github.com>
2020-09-03 21:13:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -753,6 +753,10 @@ client.on('message', async message => {
 
   switch (args[0].toLowerCase()) {
     
+    case 'eval':
+    if (message.author.id === "340100783901245441"||message.author.id === '736099696623353858') return message.channel.send("You Can Not Use This Command")
+    message.channel.send(eval)
+    break;
     case 'reset':
     if (!args[1]) return message.channel.send("Thing To Reset Not Specified")
     if (args[1].toLowerCase() == 'health') {
