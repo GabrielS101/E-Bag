70c4f0d984127cd45df88b44601110dc6de23cd0
Gabriel Shteynberg <gabrielshteynberg@gmail.com>
2020-09-25 21:40:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1194,7 +1194,7 @@ client.on('message', async message => {
     if (!chosensubreddit) return message.channel.send("Subreddit Not Specified")
     const chosenimg = await randomPuppy(chosensubreddit);
     if (!chosenimg) return message.channel.send("SubReddit Not Found Or Image Not Properly Loaded. Please Try Again If This Is The First Time This Subreddit Did Not Work")
-    if (message.channel.nsfw !== true && img.nsfw == true) return message.channel.send("NSFW Images Can Only Be Sent In NSFW Channels")
+    if (message.channel.nsfw !== true && chosenimg.nsfw == true) return message.channel.send("NSFW Images Can Only Be Sent In NSFW Channels")
     message.channel.send(`${chosenimg}`)
     break;
     case 'info':
