573b966e6bd76faa98475d6759f69ca7b03896c9
Gabriel Shteynberg <gabrielshteynberg@gmail.com>
2020-10-13 01:04:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -749,6 +749,31 @@ client.on('message', async message => {
   
   switch (args[0].toLowerCase()) {
     
+    case 'play':
+    if (!voiceChannel) return message.channel.send("Must Be In A Voice Channel To Play Music")
+    if (!permissions.has('CONNECT')) return message.channel.send("I Do Not Have Permission To Join The Voice Channel")
+    if (!permissions.has('SPEAK')) return message.channel.send("I Do Not Have Permission To Speak In The Voice Channel")
+
+    try{
+      var connection = await voiceChannel.join()
+    } catch(error) {
+      console.log(`There Was An Error Connecting To The Voice Channel: ${error}`)
+      return message.channel.send(`There Was An Error Connecting To The Voice Channel: ${error}`)
+    }
+
+    const dispatcher = connection.play(ytdl(args[1]))
+    .on('finish', () => {
+      voiceChannel.leave()
+    })
+    .on('error', error => {
+      console.log(error)
+    })
+    dispatcher.setVolumeLogarithmic(5 / 5)
+    break;
+    case 'stop':
+    if (!voiceChannel) return message.channel.send("Must Be In A Voice Channel To Play Music")
+    voiceChannel.leave()
+    break;
     case 'loan':
     var debt = db.fetch(`debt_${message.author.id}`)
     var money = db.fetch(`money_${message.author.id}`)
