775456f7caacca518efbddbdc7e54d60593b3642
Gabriel Shteynberg <gabrielshteynberg@gmail.com>
2020-10-13 21:27:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -750,11 +750,6 @@ client.on('message', async message => {
 
   switch (args[0].toLowerCase()) {
     
-    case 'restart':
-    if (message.author.id !== "340100783901245441"&&message.author.id !== '736099696623353858') return message.channel.send("You Can Not Use This Command")
-    message.channel.send("Restarted")
-    client.off()
-    break;
     case 'loan':
     var debt = db.fetch(`debt_${message.author.id}`)
     var money = db.fetch(`money_${message.author.id}`)
