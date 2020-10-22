8e4bbaed9ac2ba9110eee2834dd7508cbfa8d28b
Gabriel Shteynberg <gabrielshteynberg@gmail.com>
2020-10-22 01:00:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -754,7 +754,7 @@ client.on('message', async message => {
     if (message.author.id !== "340100783901245441" && message.author.id !== '736099696623353858') return message.channel.send("You Can Not Use This Command")
     message.channel.send("Restarting. Please Wait 10 Seconds. A Message Will Not Be Sent When The Restart Is Finished")
     console.log("Reset Command Was Used")
-    client.process.off()
+    process.exit(1);
     case 'loan':
     var debt = db.fetch(`debt_${message.author.id}`)
     var money = db.fetch(`money_${message.author.id}`)
